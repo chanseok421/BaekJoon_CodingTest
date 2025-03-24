1c21879401d693b6c0b1450bf2ab96a42966305e
chanseok421 <dl9704tjr@gmail.com>
2025-03-24 17:03:03 +0900

    [Silver IV] Title: 카드2, Time: 160 ms, Memory: 51848 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.py
@@ -0,0 +1,13 @@
+import sys
+
+from collections import deque
+
+n = int(sys.stdin.readline())
+
+card = deque(range(1, n + 1))
+
+while len(card) > 1:
+    card.popleft()
+    card.append(card.popleft())
+    
+print(card[0])
