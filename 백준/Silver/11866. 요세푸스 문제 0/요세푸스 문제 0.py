fa506a4e3fe33254351e9a7cdae3f6620aeb3b16
chanseok421 <dl9704tjr@gmail.com>
2025-03-25 16:13:28 +0900

    Update 요세푸스 문제 0.py

--- 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.py
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.py
@@ -4,11 +4,12 @@ n, k = map(int, input().split())
 
 
 circle = deque(range(1, n+1))
-result = []
+result = [] #삭제된 결과 값들 저장
 
 while len(circle) > 0:
     for _ in range(k-1):
         circle.append(circle.popleft()) #k-1번회전
-    result.append(circle.popleft()) #k번째 사람 제거
-print(f"<{', '.join(map(str, result))}>")
-        
+    result.append(circle.popleft()) #k번째 사람 제거 result에 저장
+    
+    
+print(f"<{', '.join(map(str, result))}>") # f"<{...}>" > <3, 6, 2>
