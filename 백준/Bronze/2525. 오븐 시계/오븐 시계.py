071075f41694420793df6db41a73c243034c0e31
chanseok421 <dl9704tjr@gmail.com>
2025-03-26 14:19:55 +0900

    [Bronze III] Title: 오븐 시계, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.py
@@ -0,0 +1,16 @@
+a, b = map(int, input().split())
+
+c = int(input())
+
+if b + c >= 60:
+    a += (b + c)//60
+    
+    b = (b+c)%60
+    
+    a %= 24 
+    
+else:
+    
+    b = b + c
+    
+print(a, b)
