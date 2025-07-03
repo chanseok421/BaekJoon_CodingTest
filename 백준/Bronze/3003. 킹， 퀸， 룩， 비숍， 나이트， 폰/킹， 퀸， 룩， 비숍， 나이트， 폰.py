e72927635cc29f844d9f5f9179abfe0d2592f599
chanseok421 <dl9704tjr@gmail.com>
2025-07-03 21:21:37 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.py
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.py
@@ -1,5 +1,6 @@
-piece = [1, 1, 2, 2, 2, 8]
-input = list(map(int, input().split()))
+chess = [1, 1, 2, 2, 2, 8]
 
-for i in range(len(piece)):
-  print(piece[i] - input[i], end=' ')
+myInput = list(map(int, input().split()))
+
+for i in range(len(chess)):
+  print(chess[i] - myInput[i], end = ' ')
