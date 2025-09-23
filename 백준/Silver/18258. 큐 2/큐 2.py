ef4c1965872eb7dbc0bb1b6e14f74ef84de7954e
chanseok421 <dl9704tjr@gmail.com>
2025-09-23 16:44:07 +0900

    [Silver IV] Title: 큐 2, Time: 1656 ms, Memory: 112544 KB -BaekjoonHub

--- 백준/Silver/18258. 큐 2/큐 2.py
+++ 백준/Silver/18258. 큐 2/큐 2.py
@@ -1,24 +1,43 @@
 import sys
-from collections import deque #파이썬의 deque는 양쪽에서 빠르게 삽이/삭제가 가능한 큐 자료구조 pop(0)쓰는것보다 빠르다
+from collections import deque
 
-n = int(sys.stdin.readline())        
+# 명령의 수 N을 입력받습니다.
+n = int(sys.stdin.readline())
 
-q = deque()
+# deque를 생성합니다.
+queue = deque()
 
 for _ in range(n):
-    
-    cmd = sys.stdin.readline().strip().split()
-    
-    if cmd[0] == 'push':
-        q.append(int(cmd[1]))
-    elif cmd[0] == 'pop':
-        print(q.popleft() if q else -1)
-    elif cmd[0] == 'size':
-        print(len(q))
-    elif cmd[0] == 'empty':
-        print(0 if q else 1)
-    elif cmd[0] == 'front':
-        print(q[0] if q else -1)
-    elif cmd[0] == 'back':
-        print(q[-1] if q else -1)
-         
+    # 명령어를 한 줄씩 입력받아 공백을 기준으로 나눕니다.
+    command = sys.stdin.readline().split()
+
+    if command[0] == "push":
+        queue.append(int(command[1]))
+
+    elif command[0] == "pop":
+        if not queue:
+            print(-1)
+        else:
+            # deque의 popleft()는 O(1) 시간 복잡도를 가집니다.
+            print(queue.popleft())
+
+    elif command[0] == "size":
+        print(len(queue))
+
+    elif command[0] == "empty":
+        if not queue:
+            print(1)
+        else:
+            print(0)
+
+    elif command[0] == "front":
+        if not queue:
+            print(-1)
+        else:
+            print(queue[0])
+
+    elif command[0] == "back":
+        if not queue:
+            print(-1)
+        else:
+            print(queue[-1])
