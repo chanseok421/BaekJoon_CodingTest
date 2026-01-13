536a82416e28f3cac7b714ad8bf3227806bcc0c0
chanseok421 <dl9704tjr@gmail.com>
2026-01-13 17:59:28 +0900

    [Bronze II] Title: 블랙잭, Time: 72 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.py
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -1,12 +1,26 @@
-N, M = map(int, input().split())
-lst = list(map(int, input().split()))
-nlst = []
-for i in range(N):
-    for j in range(i+1, N):
-        for k in range(j+1, N):
-            three =  lst[i] + lst[j] + lst[k]
-            if three > M:
-                continue
-            else:
-                nlst.append(three)
-print(max(nlst))
+import sys
+
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+
+cards = list(map(int, input().split()))
+
+current_m = 0
+result = 0
+
+
+    
+cards.sort()
+
+for i in range(n):
+    for j in range(i+1, n):
+        for k in range(j+1, n):
+            
+            current_m = cards[i] + cards[j] + cards[k]
+            
+            if current_m <= m and current_m > result:
+                result = current_m
+
+print(result)
+                
