b074b4e94a3d3c15c7e303f6f743b90b003017a4
chanseok421 <dl9704tjr@gmail.com>
2026-01-26 23:35:55 +0900

    [Silver IV] Title: 괄호, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.py
+++ 백준/Silver/9012. 괄호/괄호.py
@@ -1,28 +1,19 @@
-import sys
+T = int(input())
 
-t = int(sys.stdin.readline())
-
-
-
-for _ in range(t):
+for i in range(T):
     stack = []
-    cmd = sys.stdin.readline().strip()
-    
-    for ch in cmd:
-     if ch == '(':
-         stack.append(ch)
-     elif ch == ')':
-        if stack:
-            stack.pop()
-        else:
-            stack.append('x')
-            break
-         
-    if len(stack) == 0:
-        print('YES')
-    else:
-        print('NO')
-         
-    
-
-         
+    a=input()
+    for j in a:
+        if j == '(':
+            stack.append(j)
+        elif j == ')':
+            if stack:
+                stack.pop()
+            else: # 스택에 괄호가 없을경우 NO
+                print("NO")
+                break
+    else: # break문으로 끊기지 않고 수행됬을경우 수행한다
+        if not stack: # break문으로 안끊기고 스택이 비어있다면 괄호가 다 맞는거다.
+            print("YES")
+        else: # break안 걸렸더라도 스택에 괄호가 들어있다면 NO이다.
+            print("NO")
